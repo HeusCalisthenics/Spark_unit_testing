0dc54bc06026ff5c2c8e2314d93e6c827aac8f09
David Heus <david.heus@valcon.com>
2022-07-28 11:40:48 +0200

    downloaded alot

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Spark_unit_testing
+# Spark unit testing
+
+Here i will be writing my unit tests with great expectations
---
+++ unit_test.py
@@ -0,0 +1,7 @@
+import great_expectations as ge
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+
+print(df)
